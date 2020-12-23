cc23cef40a57bfa79e069176ea2adec9564a0e31
arjunjava <jadhavarjun53@gmail.com>
2020-12-23 21:30:43 +0530

    Add : check employee daily wages and merge into master

--- EmpWage.java
+++ EmpWage.java
@@ -1,13 +1,17 @@
-
 public class EmpWage {
 	public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wage ComputationProgram on Master Branch");
    	int isPresent = 1;
-      	int empCheck =(int)Math.floor(Math.random() * 10) % 2;
-      	System.out.println(empCheck);
-      		if (empCheck == isPresent)
-      		System.out.println("present");
-      		else
-      		System.out.println("absent");
+	int empRatePrHr = 20;
+	int empHr = 8;
+    int empCheck=(int)Math.floor(Math.random() * 10) % 2;
+    System.out.println(empCheck);
+    if (empCheck == isPresent) {
+		System.out.println("employee is present");
+		int perDaySalary = empRatePrHr * empHr;
+		System.out.println("Employee per day salary = "+perDaySalary);
+	}
+    else
+		System.out.println("absent");
 	}
 }
