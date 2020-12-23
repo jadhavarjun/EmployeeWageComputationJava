a8c7b85dc08532f1736eef097ab17e876ba844f8
arjunjava <jadhavarjun53@gmail.com>
2020-12-23 22:02:21 +0530

    Add : to calculate employee wages till condition of total working hours or days is reached for a month

--- EmpWage.java
+++ EmpWage.java
@@ -1,6 +1,6 @@
 public class EmpWage {
-	public static final int isFullTime = 1;
-        public static final int isPartTime = 2;
+	public static final int isFullTime = 0;
+        public static final int isPartTime = 1;
 	public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wage ComputationProgram on Master Branch");
 		int empRatePrHr = 20;
@@ -8,29 +8,29 @@ public class EmpWage {
 		int perDaySalary = 0;
 		int mothWorkingDay = 20;
 		int monthlySalary = 0;
-		for (int i = 1;i <= mothWorkingDay;i++)
-		{
-			double empCheck = Math.floor(Math.random() * 10) % 3;
+		int monthyHrs = 0;
+		for (int i = 1;i <= mothWorkingDay && monthyHrs < 100;i++) {
+			int empCheck=(int)Math.floor(Math.random() * 10) % 2;
 			switch ((int)empCheck)
 			{
 				case isFullTime :
 					empHr = 8;
 					break;
-
 				case isPartTime :
 					empHr = 4;
 					break;
-				default :
-					empHr = 0;
-					break;
 
 			}//switch
+			monthyHrs = monthyHrs + empHr;
+			System.out.println("working days : "+i);
+			System.out.println("monthyHrs wis : "+monthyHrs);
 			perDaySalary = empRatePrHr * empHr;
-
+			System.out.println(perDaySalary);
 			monthlySalary = perDaySalary + monthlySalary;
 
+
 		}//forloop
-		System.out.println("Monthly salary: "+monthlySalary);
+		System.out.println("monthly salary"+monthlySalary);
 
 	}//main()
 }//class
