1044850e963d3a7982f7c759b31c05519d5685d9
arjunjava <jadhavarjun53@gmail.com>
2020-12-23 21:38:40 +0530

    Add : check employee full time and part time daily wages

--- EmpWage.java
+++ EmpWage.java
@@ -1,17 +1,31 @@
 public class EmpWage {
 	public static void main(String[] args) {
-	System.out.println("Welcome to Employee Wage ComputationProgram on Master Branch");
-   	int isPresent = 1;
-	int empRatePrHr = 20;
-	int empHr = 8;
-    int empCheck=(int)Math.floor(Math.random() * 10) % 2;
-    System.out.println(empCheck);
-    if (empCheck == isPresent) {
-		System.out.println("employee is present");
-		int perDaySalary = empRatePrHr * empHr;
-		System.out.println("Employee per day salary = "+perDaySalary);
-	}
-    else
-		System.out.println("absent");
-	}
-}
+		System.out.println("Welcome to Employee Wage ComputationProgram on Master Branch");
+		int isFullTime = 1;
+		int isPartTime = 2;
+		int empRatePrHr = 20;
+		int empHr = 0;
+		int perDaySalary = 0;
+		int empCheck=(int)Math.floor(Math.random() * 10) % 3;
+		System.out.println(empCheck);
+		switch ((int)empCheck)
+		{
+			case 1 :
+				empHr = 8;
+				System.out.println("Full time Employee");
+				perDaySalary = empRatePrHr * empHr;
+				System.out.println("Employee per day salary = "+perDaySalary);
+				break;
+			case 2 :
+				empHr = 4;
+				System.out.println("Part time Employee");
+				perDaySalary = empRatePrHr * empHr;
+				System.out.println("Employee per day salary = "+perDaySalary);
+				break;
+			default :
+				empHr = 0;
+				perDaySalary = 0;
+				System.out.println("Employee per day salary = "+perDaySalary);
+		}//switch
+	}//main()
+}//class
