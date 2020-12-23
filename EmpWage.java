a2ce15ae1ffc72f71c54a837460293040740afd0
arjunjava <jadhavarjun53@gmail.com>
2020-12-23 21:12:14 +0530

    Add : check employee present or not and merge into master

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,13 @@
-public class EmpWage
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Welcome to Employee Wage ComputationProgram on Master Branch");
+
+public class EmpWage {
+	public static void main(String[] args) {
+	System.out.println("Welcome to Employee Wage ComputationProgram on Master Branch");
+   	int isPresent = 1;
+      	int empCheck =(int)Math.floor(Math.random() * 10) % 2;
+      	System.out.println(empCheck);
+      		if (empCheck == isPresent)
+      		System.out.println("present");
+      		else
+      		System.out.println("absent");
 	}
 }
