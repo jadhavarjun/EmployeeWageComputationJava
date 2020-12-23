d4c4da56884fb04d93a5a7aed697ec36819ff71b
arjunjava <jadhavarjun53@gmail.com>
2020-12-23 21:53:46 +0530

    Add : to check employee wage for month and merge into master

--- EmpWage.java
+++ EmpWage.java
@@ -6,26 +6,31 @@ public class EmpWage {
 		int empRatePrHr = 20;
 		int empHr = 0;
 		int perDaySalary = 0;
-		int empCheck=(int)Math.floor(Math.random() * 10) % 3;
-		System.out.println(empCheck);
-		switch ((int)empCheck)
+		int mothWorkingDay = 20;
+		int monthlySalary = 0;
+		for (int i = 1;i <= mothWorkingDay;i++)
 		{
-			case isFullTime :
-				empHr = 8;
-				System.out.println("Full time Employee");
-				perDaySalary = empRatePrHr * empHr;
-				System.out.println("Employee per day salary = "+perDaySalary);
-				break;
-			case isPartTime :
-				empHr = 4;
-				System.out.println("Part time Employee");
-				perDaySalary = empRatePrHr * empHr;
-				System.out.println("Employee per day salary = "+perDaySalary);
-				break;
-			default :
-				empHr = 0;
-				perDaySalary = 0;
-				System.out.println("Employee per day salary = "+perDaySalary);
-		}//switch
+			double empCheck = Math.floor(Math.random() * 10) % 3;
+			switch ((int)empCheck)
+			{
+				case isFullTime :
+					empHr = 8;
+					break;
+
+				case isPartTime :
+					empHr = 4;
+					break;
+				default :
+					empHr = 0;
+					break;
+
+			}//switch
+			perDaySalary = empRatePrHr * empHr;
+
+			monthlySalary = perDaySalary + monthlySalary;
+
+		}//forloop
+		System.out.println("Monthly salary: "+monthlySalary);
+
 	}//main()
 }//class
